a372d4a58cba09b4672398fb52ef9f37cea6bd23
dhowett <dhowett@29de6bd3-9c77-4744-869b-932cb95d1490>
2010-11-30 03:33:13 +0000

    [install] Update for variable reform, and automatically determine the number of jobs to use.
    git-svn-id: http://svn.howett.net/svn/theos-modules/trunk@5 29de6bd3-9c77-4744-869b-932cb95d1490

--- install/master/rules.mk
+++ install/master/rules.mk
@@ -1,16 +1,16 @@
-ifeq ($(FW_DEVICE_IPS),)
+ifeq ($(THEOS_DEVICE_IPS),)
 installs:
-	@echo "make installs requires that you set FW_DEVICE_IPS to a space-delimited list of devices to install to."; exit 1
+	@echo "make installs requires that you set THEOS_DEVICE_IPS to a space-delimited list of devices to install to."; exit 1
 else
 installs:
-	@$(MAKE) --no-print-directory --no-keep-going mod-internal-installs _FW_MAKE_PARALLEL=yes _FW_DEVICE_IPS="$(FW_DEVICE_IPS)"
+	@$(MAKE) -j$(words $(THEOS_DEVICE_IPS)) --no-print-directory --no-keep-going mod-internal-installs _THEOS_MAKE_PARALLEL=yes _THEOS_DEVICE_IPS="$(THEOS_DEVICE_IPS)"
 endif
 
-mod-internal-installs: $(foreach i,$(_FW_DEVICE_IPS),$(i).install)
+mod-internal-installs: $(foreach i,$(_THEOS_DEVICE_IPS),$(i).install)
 
 %.install:
 	@ \
 	in="$@"; \
 	ip=$${in%.install}; \
 	echo Making install for $$ip; \
-	$(MAKE) --no-print-directory --no-keep-going install FW_DEVICE_IP=$$ip _FW_TOP_INVOCATION_DONE=""
+	$(MAKE) --no-print-directory --no-keep-going install THEOS_DEVICE_IP=$$ip _THEOS_TOP_INVOCATION_DONE=""
