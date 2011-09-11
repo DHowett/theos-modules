6965c4fdd0e918dc7438c92024019a3e5fd223a7
Dustin L. Howett <dustin@howett.net>
2011-09-11 16:58:28 -0700

    [congruence] Add support for CoreData data models, specified in xxx_DATA_MODELS. Uses momc. Switch to xcrun for ibtool.

--- congruence/instance/rules.mk
+++ congruence/instance/rules.mk
@@ -2,8 +2,12 @@ NIB_FILES = $(strip $(patsubst %.xib,%.nib,$($(THEOS_CURRENT_INSTANCE)_XIBS)))
 NIB_FILES_TO_USE = $(strip $(addprefix $(THEOS_OBJ_DIR)/,$(NIB_FILES)))
 _NIB_DIR_STAMPS = $(sort $(foreach o,$(filter $(THEOS_OBJ_DIR)%,$(NIB_FILES_TO_USE)),$(dir $o).stamp))
 
-ifneq ($(NIB_FILES_TO_USE),)
-after-$(THEOS_CURRENT_INSTANCE)-all:: $(_NIB_DIR_STAMPS) $(NIB_FILES_TO_USE)
-after-$(THEOS_CURRENT_INSTANCE)-stage:: $(NIB_FILES_TO_USE)
-	@cp $(NIB_FILES_TO_USE) $(THEOS_SHARED_BUNDLE_RESOURCE_PATH)/
+DATA_MODELS = $(strip $(patsubst %.xcdatamodeld,%.momd,$($(THEOS_CURRENT_INSTANCE)_DATA_MODELS)))
+DATA_MODELS_TO_USE = $(strip $(addprefix $(THEOS_OBJ_DIR)/,$(DATA_MODELS)))
+_DATA_MODEL_DIR_STAMPS = $(sort $(foreach o,$(filter $(THEOS_OBJ_DIR)%,$(DATA_MODELS_TO_USE)),$(dir $o).stamp))
+
+ifneq ($(NIB_FILES_TO_USE)$(DATA_MODELS_TO_USE),)
+after-$(THEOS_CURRENT_INSTANCE)-all:: $(_NIB_DIR_STAMPS) $(NIB_FILES_TO_USE) $(_DATA_MODEL_DIR_STAMPS) $(DATA_MODELS_TO_USE)
+after-$(THEOS_CURRENT_INSTANCE)-stage:: $(NIB_FILES_TO_USE) $(DATA_MODELS_TO_USE)
+	@cp -a $(DATA_MODELS_TO_USE) $(NIB_FILES_TO_USE) $(THEOS_SHARED_BUNDLE_RESOURCE_PATH)/
 endif
--- congruence/rules.mk
+++ congruence/rules.mk
@@ -1,2 +1,5 @@
 $(THEOS_OBJ_DIR)/%.nib: %.xib
-	$(ECHO_COMPILING)ibtool --compile $@ $<$(ECHO_END)
+	$(ECHO_COMPILING)xcrun -sdk iphoneos ibtool --compile "$@" "$<"$(ECHO_END)
+
+$(THEOS_OBJ_DIR)/%.momd: %.xcdatamodeld
+	$(ECHO_COMPILING)xcrun -sdk iphoneos momc -XD_MOMC_SDKROOT="$(SYSROOT)" -XD_MOMC_IOS_TARGET_VERSION=$(_THEOS_TARGET_SDK_VERSION) -MOMC_PLATFORMS iphoneos -XD_MOMC_TARGET_VERSION="10.6" "$(shell unset CDPATH; cd $(dir $<); pwd)/$(notdir $<)" "$(shell unset CDPATH; cd $(dir $@); pwd)/$(notdir $@)"$(ECHO_END)
